bdcba785eece1e5e450e54e2800a2b2e34fde116
Kalnov Alexey <kalnovalexey@yandex.ru>
2014-02-18 13:35:45 +0400

    fix att_remove_all

--- attach2/inc/attach2.functions.php
+++ attach2/inc/attach2.functions.php
@@ -448,9 +448,9 @@ function att_remove_all($user_id = null, $area = null, $item_id = null)
 	$count = $sql->rowCount();
 	foreach ($sql->fetchAll() as $row)
 	{
-		unlink($row['att_path']);
+		@unlink($row['att_path']);
 		att_remove_thumbs($row['att_id']);
-		rmdir($cfg['plugin']['attach2']['folder'] . '/_thumbs/' . $row['att_id']);
+		@rmdir($cfg['plugin']['attach2']['folder'] . '/_thumbs/' . $row['att_id']);
 	}
 	$db->delete($db_attach, $where);
 
